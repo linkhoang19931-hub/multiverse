99369acd962d5bd215ae1a574483ee08a834f861
linkhoang19931-hub <linkhoang19931@gmail.com>
2025-12-11 16:05:47 +0700

    Create characters.py

---
+++ data/characters.py
@@ -0,0 +1,16 @@
+[
+  {
+    "name": "Naruto",
+    "series": "Naruto",
+    "x": 800,
+    "y": 500,
+    "sprite": "https://i.imgur.com/3Q7ZKzG.png"
+  },
+  {
+    "name": "Luffy",
+    "series": "One Piece",
+    "x": 1200,
+    "y": 600,
+    "sprite": "https://i.imgur.com/FvQFf2V.png"
+  }
+]
